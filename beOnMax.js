6a142ba49b6414b2fe420c3544ddbc381c3e018e
Anna <petrovaanna-2015@mail.ru>
2024-10-22 17:09:18 +0300

    first_commit

---
+++ beOnMax.js
@@ -0,0 +1,60 @@
+
+'use strict'
+
+// var LeftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+// console.log(second);
+
+
+// console.log(4/0);
+// console.log('string'*9);
+// let something;
+// console.log(something);
+
+// let person = {
+//     name:"John",
+//     age:25,
+//     isMariied:false
+// };
+// console.log(person.name);
+
+// let arr = ['plun.png', 'orange.jpg', 'apple.bnp'];
+// console.log(arr[0]);
+
+// alert("Hello word!");
+
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+
+               //Операторы в JS
+
+           
+// console.log(4 + + "-object");               
+
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);   
+
+a = 5;
+console.log(a);
